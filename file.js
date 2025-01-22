7f4746d77a490f5b79097484fea328e67b06ba55
Ismail S <ismailpromt2i@gmail.com>
2025-01-23 00:02:12 +0530

    Update file.js
    fg

--- file.js
+++ file.js
@@ -1,10 +1,10 @@
 const fs = require("fs")
 const os = require('os')
 
-// console.log(os.cpus().length);
+console.log(os.cpus().length);
 
 
-// console.log("1");
+console.log("1");
 
 
  fs.readFile("contact.txt", "utf-8" , (err , result) =>{
@@ -16,30 +16,30 @@ const os = require('os')
 
 
 
-// fs.writeFileSync("./text.txt","hey Buddy" )
+fs.writeFileSync("./text.txt","hey Buddy" )
 
-// fs.writeFile("./text.txt","hey d", (err)=>{})
+fs.writeFile("./text.txt","hey d", (err)=>{})
 
-// //     const result = fs.readFileSync('./contact.txt', "utf-8")
+    const result = fs.readFileSync('./contact.txt', "utf-8")
 
-// //   console.log(result);
+  console.log(result);
   
-// // fs.readFile('./contact.txt', "utf-8",(err , result) => {
-// //     if(err){
-// //         console.log("error", err);
+fs.readFile('./contact.txt', "utf-8",(err , result) => {
+    if(err){
+        console.log("error", err);
         
-// //     }else{
-// //         console.log(result);
+    }else{
+        console.log(result);
         
-// //     }
-// // })
-// // fs.unlinkSync( "./copy1")
+    }
+})
+fs.unlinkSync( "./copy1")
 
-// // fs.appendFileSync("./text.txt", `${Date.now()} hey Jarvis\n`);
+fs.appendFileSync("./text.txt", `${Date.now()} hey Jarvis\n`);
 
 
-// // fs.copyFileSync("./text.txt", "./copy1")
-//  console.log( fs.statSync("./text.txt").isFile());
+fs.copyFileSync("./text.txt", "./copy1")
+ console.log( fs.statSync("./text.txt").isFile());
 
-// //  fs.mkdirSync("my-docf/a/b", {recursive:true})
+ fs.mkdirSync("my-docf/a/b", {recursive:true})
 
