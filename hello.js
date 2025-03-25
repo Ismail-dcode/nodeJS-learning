e1a2f92ffe2e8ad81669d87628239ae5565aae0d
Ismail S <ismailpromt2i@gmail.com>
2025-03-26 03:27:59 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,3 +3,38 @@ const math = require("./math");
 
 console.log("Here is your Answer", math.sub(6 , 2));
 
+const math = require("./math");
+
+
+console.log("Here is your Answer", math.sub(6 , 2));
+
+const math = require("./math");
+
+
+console.log("Here is your Answer", math.sub(6 , 2));
+
+const math = require("./math");
+
+
+console.log("Here is your Answer", math.sub(6 , 2));
+
+const math = require("./math");
+
+
+console.log("Here is your Answer", math.sub(6 , 2));
+
+const math = require("./math");
+
+
+console.log("Here is your Answer", math.sub(6 , 2));
+
+const math = require("./math");
+
+
+console.log("Here is your Answer", math.sub(6 , 2));
+
+const math = require("./math");
+
+
+console.log("Here is your Answer", math.sub(6 , 2));
+
